e38dd9d2ee989af252b158f0d5b01301fc677c4d
cade.gardner <d00299297@dmail.dixie.edu>
2020-07-23 09:49:14 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 6 */
+
+var app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: {
+        wildflowers: [
+            {}
+        ],
+        trees: [],
+    
+    },
+    created: {
+
+    },
+    methods: {
+
+    }
+});
